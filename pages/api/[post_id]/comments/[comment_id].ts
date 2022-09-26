715e92717ebb6b9bd50957e75944e5b12d773ca6
Kieran Singh <kieransingh27@gmail.com>
2022-09-26 12:30:08 +1000

    Added routes to create, read, update and delete comments - update and delete routes subject to testing

--- pages/api/[post_id]/comments/[comment_id].ts
+++ pages/api/[post_id]/comments/[comment_id].ts
@@ -3,13 +3,72 @@ import { authenticate } from "../../../../api - lib/middleware/authentication";
 import dbConnect from "../../../../api - lib/middleware/mongo_connect";
 import post from "../../../../api - lib/models/post";
 import nextConnect from "next-connect";
+import comment from "../../../../api - lib/models/comment";
+import user from "../../../../api - lib/models/user";
 
 const handler = nextConnect();
 
-handler.get(async (req: NextApiRequest, res: NextApiResponse) => {
-  // Connect to db
-  await dbConnect();
-  return res.json({ puffin: "asmr" });
-});
+handler
+  .use(authenticate)
+  // Update selected comment
+  .get(async (req: NextApiRequest, res: NextApiResponse) => {
+    // Connect to db
+    await dbConnect();
+
+    // Query post_id and comment_id from req params
+    const query = req.query;
+    const { comment_id } = query;
+
+    // Get updated content and user_id from req body
+    const { content } = req.body;
+
+    // Update comment with updated content and updated timestamp
+    try {
+      const updatedComment = comment.findByIdAndUpdate(
+        comment_id,
+        {
+          content: content,
+          lastUpdatedAt: new Date().toISOString(),
+        },
+        async (error: any, updatedComment: any) => {
+          if (error) {
+            throw new Error(error);
+          }
+          return res.status(200).json({ newComment: comment });
+        }
+      );
+      return res.status(200).json({ updatedComment: updatedComment });
+    } catch (error) {
+      return res.status(409).json({ error });
+    }
+  })
+
+  // Delete selected comment
+  .post(async (req: NextApiRequest, res: NextApiResponse) => {
+    // Connect to db
+    await dbConnect();
+    // Validate req body
+
+    // Query post_id from req params
+    const query = req.query;
+    const { post_id, comment_id } = query;
+
+    // Get user_id from req.body
+    const { user_id } = req.body;
+
+    // Delete comment and remove comment id from user's comments array and posts comments array
+    try {
+      await comment.findByIdAndDelete(comment_id);
+      await user.findByIdAndUpdate(user_id, {
+        $pull: { comments: comment_id },
+      });
+      await post.findByIdAndUpdate(post_id, {
+        $pull: { comments: comment_id },
+      });
+      return res.status(200).json({ message: `Comment ${comment_id} Deleted` });
+    } catch (error) {
+      return res.status(409).json({ error });
+    }
+  });
 
 export default handler;
--- pages/api/[post_id]/comments/index.ts
+++ pages/api/[post_id]/comments/index.ts
@@ -3,23 +3,26 @@ import { authenticate } from "../../../../api - lib/middleware/authentication";
 import dbConnect from "../../../../api - lib/middleware/mongo_connect";
 import post from "../../../../api - lib/models/post";
 import nextConnect from "next-connect";
+import comment from "../../../../api - lib/models/comment";
+import user from "../../../../api - lib/models/user";
 
 const handler = nextConnect();
 
 handler
   .use(authenticate)
+  // Get All Comments of post
   .get(async (req: NextApiRequest, res: NextApiResponse) => {
     // Connect to db
     await dbConnect();
-    // Query database to find posts and return as JSON
-    /*
-    Extra -- Find Posts whose author ID matches User Id and posts
-    that user is friends with
-    */
-    post
-      .find()
+
+    // Query post_id from req params
+    const query = req.query;
+    const { post_id } = query;
+
+    comment
+      .find({ postId: post_id })
       .sort({ timestamp: "descending" })
-      .populate("comments")
+      .populate({ path: "author", model: user, select: "username" })
       .exec((err, data) => {
         if (err) {
           return res.status(404).json({ error: err });
@@ -28,21 +31,46 @@ handler
       });
   })
 
-  // Create New Post
+  // Create new comment
 
   .post(async (req: NextApiRequest, res: NextApiResponse) => {
     // Connect to db
     await dbConnect();
     // Validate req body
+
+    // Query post_id from req params
+    const query = req.query;
+    const { post_id } = query;
+
+    // Get user_id from req.body
     const { content, user_id } = req.body;
-    console.log(content, user_id);
+
+    // Create new comment, grab its id within callback and update user's comments array
     try {
-      const newPost = post.create({
-        author: user_id,
-        content: content,
-        timestamp: new Date().toISOString(),
-      });
-      return res.status(200).json({ newPost: newPost });
+      const newComment = comment.create(
+        {
+          $push: { author: user_id },
+          content: content,
+          lastUpdatedAt: new Date().toISOString(),
+          timestamp: new Date().toISOString(),
+          postId: post_id,
+        },
+        async (error: any, comment: any) => {
+          if (error) {
+            throw new Error(error);
+          }
+          const newlyCreatedCommentId = comment._id;
+          // Add newlyCreatedCommentId to user's comments array
+          await user.findByIdAndUpdate(user_id, {
+            $push: { comments: newlyCreatedCommentId },
+          });
+          // Add newlyCreatedCommentId to original post's comment array
+          await post.findByIdAndUpdate(post_id, {
+            $push: { comments: newlyCreatedCommentId },
+          });
+          return res.status(200).json({ newComment: comment });
+        }
+      );
     } catch (error) {
       return res.status(409).json({ error });
     }
